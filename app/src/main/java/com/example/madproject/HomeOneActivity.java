91dd2f0e0aca1ee8eb5d5451f342715c08d1a062
RafiaShakeel <rafii.shakeel@gmail.com>
2024-05-15 14:44:52 +0500

    SplashScreen
    -Created Splash Screen
    -Implemented the splash logic
    -HomeOneActivity.class was created to make the first home page that contains the login or sign up form logics will be implemented later on.

---
+++ app/src/main/java/com/example/madproject/HomeOneActivity.java
@@ -0,0 +1,18 @@
+package com.example.madproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeOneActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_one);
+
+    }
+}
--- app/src/main/java/com/example/madproject/MainActivity.java
+++ app/src/main/java/com/example/madproject/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.madproject;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY = 3000; // 3 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Delay transition to next activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start the next activity
+                Intent intent = new Intent(MainActivity.this, HomeOneActivity.class);
+                startActivity(intent);
+                finish(); // Finish the splash activity
+            }
+        }, SPLASH_DELAY);
     }
 }
