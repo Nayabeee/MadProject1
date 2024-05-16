bf92f49a01aefcfbb40fe8f35476c66b9b91c396
uswa9999 <153259809+uswa9999@users.noreply.github.com>
2024-05-16 22:35:30 +0500

    added intents for signin
    -added logic for intent
    -created a new page for sign in

--- app/src/main/java/com/example/madproject/HomeOneActivity.java
+++ app/src/main/java/com/example/madproject/HomeOneActivity.java
@@ -3,6 +3,8 @@ package com.example.madproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +15,17 @@ public class HomeOneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_one);
+
+
+        Button SignIn=findViewById(R.id.signin);
+        SignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (getApplicationContext(),SignIn.class);
+                startActivity(intent);
+
+            }
+        });
 
     }
 }
